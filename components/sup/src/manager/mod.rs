93148fa6e956f590dc9f63bae7582b9e4dcb0b61
Christopher Maier <cmaier@chef.io>
2019-03-22 17:15:27 -0400

    Remove last bits of "signal"-ness from Windows
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/manager/mod.rs
+++ components/sup/src/manager/mod.rs
@@ -824,10 +824,20 @@ impl Manager {
             if self.check_for_departure() {
                 break ShutdownMode::Departed;
             }
+
             if !feat::is_enabled(feat::IgnoreSignals) {
-                if let Some(SignalEvent::Passthrough(Signal::HUP)) = signals::check_for_signal() {
-                    outputln!("Supervisor shutting down for signal");
-                    break ShutdownMode::Restarting;
+                // Normally, we'd use an `if let Some(...)`
+                // formulation here, but we can't add `#cfg[...]`
+                // annotations to `if` expressions right now.
+                //
+                // We *can* add them to match arms, though!
+                match signals::check_for_signal() {
+                    #[cfg(unix)] // Passthrough SignalEvents aren't a thing on Windows
+                    Some(SignalEvent::Passthrough(Signal::HUP)) => {
+                        outputln!("Supervisor shutting down for signal");
+                        break ShutdownMode::Restarting;
+                    }
+                    _ => {}
                 }
             }
 
